1169227e0923b82051f5cc7da61e7072355f7456
dextter1913 <dextter1913@gmail.com>
2021-06-17 13:52:44 -0500

    Estructurando Base de datos

--- db.sql
+++ db.sql
@@ -1,10 +1,31 @@
-CREATE DATABASE IF NOT EXISTS trello;
+CREATE DATABASE IF NOT EXISTS Actividades;
 
-USE trello;
-
+USE Actividades;
+CREATE TABLE Usuarios(
+    user VARCHAR (50) PRIMARY KEY NOT NULL,
+    pass VARCHAR (50) NOT NULL
+);
 CREATE TABLE empleados(
     doc VARCHAR (50) PRIMARY KEY,
     nombre VARCHAR (50) NOT NULL,
     apellido VARCHAR (50) NOT NULL,
-    telefono VARCHAR (50)
-)
+    telefono VARCHAR (50) NOT NULL,
+    user VARCHAR (50) NOT NULL,
+    INDEX(user),
+    FOREIGN KEY empleado(user) REFERENCES Usuarios(user)
+);
+CREATE TABLE actividades(
+    idActividad INT AUTO_INCREMENT PRIMARY KEY,
+    descripcionActividad TEXT NOT NULL
+);
+CREATE TABLE tiempos(
+    idtiempo INT AUTO_INCREMENT PRIMARY KEY,
+    fecActividad DATE NOT NULL,
+    cantHoras INT (3) NOT NULL,
+    user VARCHAR (50) NOT NULL,
+    idActividad INT NOT NULL,
+    INDEX(idActividad),
+    FOREIGN KEY tiempos(idActividad) REFERENCES actividades(idActividad),
+    INDEX(user),
+    FOREIGN KEY tiempo(user) REFERENCES Usuarios(user)
+);
