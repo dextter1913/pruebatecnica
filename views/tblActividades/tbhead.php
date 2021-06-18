ab2467e2818376ea16f0fae96528c9c9842064d7
dextter1913 <dextter1913@gmail.com>
2021-06-17 22:53:07 -0500

    Recuperando el form de la tbhead

--- views/tblActividades/tbhead.php
+++ views/tblActividades/tbhead.php
@@ -1,13 +1,14 @@
 <div class="container">
-    <div class="row table-responsive shadow p-3 mb-5 bg-body text-center">
-        <h1 class="Actividades">Actividades</h1>
-        <table class="table table-hover rounded">
-            <thead>
-                <tr>
-                    <th>Seleccionar</th>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th>Actividad</th>
-                </tr>
-            </thead>
-            <tbody>
+        <div class="row table-responsive shadow p-3 mb-5 bg-body text-center">
+            <h1 class="Actividades">Actividades</h1>
+            <table class="table table-hover rounded">
+                <thead>
+                    <tr>
+                        <th>Seleccionar</th>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Actividad</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <form action="index.php?modulos=tiempos" method="post">
