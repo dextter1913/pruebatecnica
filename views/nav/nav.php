bf2361dbb5eb4255b76f68b4c6b184180112b38f
dextter1913 <dextter1913@gmail.com>
2021-06-17 21:02:41 -0500

    cambiando color el nav

--- views/nav/nav.php
+++ views/nav/nav.php
@@ -1,19 +1,19 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-dark text-white">
     <div class="container-fluid">
-        <a class="navbar-brand" href="?modulos=ver">Bienvenido <?= $_SESSION['logeado'];?></a>
+        <a class="navbar-brand text-white" href="?modulos=ver">Bienvenido <?= $_SESSION['logeado'];?></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="?modulos=ver">Ver</a>
+                    <a class="nav-link text-white" aria-current="page" href="?modulos=ver">Ver</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="?modulos=crear">Crear</a>
+                    <a class="nav-link text-white" href="?modulos=crear">Crear</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="?modulos=tiempos">Tiempos</a>
+                    <a class="nav-link text-white" href="?modulos=tiempos">Tiempos</a>
                 </li>
             </ul>
         </div>
