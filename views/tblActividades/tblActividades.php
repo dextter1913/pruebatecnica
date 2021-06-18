6b6e62d3e6cb94268d2a97434ac34a7156f3fd70
dextter1913 <dextter1913@gmail.com>
2021-06-17 21:11:38 -0500

    Agregando titulo

--- views/tblActividades/tblActividades.php
+++ views/tblActividades/tblActividades.php
@@ -35,11 +35,18 @@
                     </tr>
                 </tfoot>
             </table>
-            <div class="col-12 col-sm-6 col-md-6 col-lg-6">
+            <div class="col-12 col-sm-6 col-md-6 col-lg-6"><br/>
+                <h3>Registro de horas</h3>
+            </div>
+            <div class="col-12 col-sm-6 col-md-3 col-lg-3">
                 <label for="fecha">Ingrese Fecha</label>
                 <input type="date" name="fecha" class="form-control" id="fecha">
             </div>
-            <div class="col-12 col-sm-6 col-md-6 col-lg-6"></div>
+            <div class="col-12 col-sm-6 col-md-3 col-lg-3">
+                <label for="horas">Horas</label>
+                <input type="number" name="horas" id="horas" class="form-control"><br />
+                <input type="submit" value="Ingresar" class="btn btn-outline-secondary btn-sm">
+            </div>
             </form>
         </div>
     </div>
